7ff79e965de8ebb4efd6c80ab8733eec792845a2
xuhuofei <xuhuofei@thewesthill.net>
2024-07-02 18:19:10 +0800

    [feat]用户信息模块

--- routers/router.go
+++ routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	v1 "ginblog/api/v1"
 	setting "ginblog/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -9,13 +10,15 @@ func InitRouter() {
 	gin.SetMode(setting.AppMode)
 	r := gin.Default()
 
+	/*
+		前端展示页面接口
+	*/
 	router := r.Group("api/v1")
 	{
-		router.GET("hello", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "hello world",
-			})
-		})
+		// 用户信息模块
+		router.POST("user/add", v1.AddUser)
+		router.GET("user/:id", v1.GetUserInfo)
+		router.GET("users", v1.GetUsers)
 	}
 
 	err := r.Run(setting.HttpPort)
